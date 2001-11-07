d13964cac0b45327f1eeac67eb90e165acdcee88
Gengbin Zheng <gzheng@illinois.edu>
2001-11-07 06:28:48 +0000

    makeEven always return 0 if input is less than 1000.

--- projections/gui/U.java
+++ projections/gui/U.java
@@ -34,7 +34,12 @@ public class U
 	}
 	//Makes i an "even" integer-- a near multiple of 10, or 5, or 25
 	public static long makeEven(long i) {
-		return (i/1000)*1000;
+		if (i>1000)
+		  return (i/1000)*1000;
+                else if (i>10)
+		  return (i/10)*10;
+                else 
+		  return i;
 	}
 	//Print nDec digits of the decimal expansion of
 	// the fraction part of d (which must be positive).

10c83edbc771c5a45c78d358a529ac8e5f47aa24
Chee Wai Lee <cheelee@illinois.edu>
2002-07-01 21:22:17 +0000

    Base abstract class for all toolkit windows in order to use the base Range Dialog class

---
+++ projections/gui/ProjectionsWindow.java
@@ -0,0 +1,37 @@
+package projections.gui;
+
+import projections.gui.*;
+
+import java.awt.*;
+
+/**
+ *  ProjectionsWindow
+ *  written by Chee Wai Lee
+ *  6/28/2002
+ *
+ *  This class should be inherited by all projections tools that present
+ *  some kind of main window and show a dialog box requiring the user to
+ *  input a range of processors and a time interval.
+ *
+ *  The author would need to implement the following methods to support
+ *  an interface to work with the basic RangeDialog object which presents
+ *  to the user a dialog interface.
+ *
+ *  The author would also need to implement any extra data setting fields
+ *  for the specific projections tool to interface correctly with the
+ *  corresponding dialog (which should be a subclass of RangeDialog).
+ *
+ */
+
+public abstract class ProjectionsWindow 
+    extends Frame
+{
+    private RangeDialog dialog;
+
+    abstract void showDialog();
+
+    public abstract void setProcessorRange(OrderedIntList validPEs);
+    public abstract void setStartTime(long time);
+    public abstract void setEndTime(long time);
+}
+

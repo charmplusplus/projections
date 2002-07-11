88ae23cfd6239982875d3292536b3087a7f8310e
Joshua Unger <unger1@uiuc.edu>
2002-07-11 05:53:47 +0000

    For screen coords

---
+++ projections/gui/graph/Coordinate.java
@@ -0,0 +1,10 @@
+package projections.gui.graph;
+
+/** Simply an x-y double. */
+public class Coordinate {
+  double x = 0.0;
+  double y = 0.0;
+
+  Coordinate() { }
+  Coordinate(double x_, double y_) { x=x_; y=y_; }
+}

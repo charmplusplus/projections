9d9b24a74eb34761e73daaf0741368a210377dc7
Joshua Unger <unger1@uiuc.edu>
2002-07-14 06:27:36 +0000

    This lets a number be formatted nicely (specify digits, add commas) AND be sorted by a TableSorter

---
+++ projections/gui/FormattedNumber.java
@@ -0,0 +1,19 @@
+package projections.gui;
+import java.text.NumberFormat;
+
+  /** To enable the correct format for both int and scientific. */
+  public class FormattedNumber extends Number {
+    double       number;
+    NumberFormat format;
+    public FormattedNumber(double d, NumberFormat f) {
+      number = d;
+      format = f;
+    }
+    public byte byteValue() { return (byte) number; }
+    public double doubleValue() { return number; }
+    public float floatValue() { return (float) number; }
+    public int intValue() { return (int) number; }
+    public long longValue() { return (long) number; }
+    public short shortValue() { return (short) number; }
+    public String toString() { return format.format(number); }
+  }

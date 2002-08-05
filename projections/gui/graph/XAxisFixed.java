d833515fa25f7af7deb01fcada403f16d5cfd06c
Sindhura Bandhakavi <sindhura.bandhaka@gmail.com>
2002-08-05 20:30:56 +0000

    added multiplier for more flexible display of XAxis labels

--- projections/gui/graph/XAxisFixed.java
+++ projections/gui/graph/XAxisFixed.java
@@ -10,14 +10,36 @@ public class XAxisFixed extends XAxis
 {
   private String title;
   private String units;
+
+// for XAxis starting with a value other than 0. default 0
+  private double start = 0;
+  private double multiplier = 1;
+
   public XAxisFixed(String title_,String units_) {
     title=title_; units=units_;
   }
-  
-  public String getTitle() {return title;}
-  public String getIndexName(int index) {
-    return ""+index+units;
+
+  public void setLimits(double start_, double multiplier_){  
+    start = start_;	
+    multiplier = multiplier_;
   }
+
+  public String getTitle() {return title;}
+  public String getUnits() {return units;}
+  public String getIndexName(int index) {
+	double temp = (index * multiplier) + start;
+        return ""+temp+units;
+  }
+
+  public double getIndex(int index) {
+	double temp = (index * multiplier) + start;
+        return temp;
+  }
+ 
+  public double getMultiplier(){
+	return multiplier;
+  }
+
 }
 
 

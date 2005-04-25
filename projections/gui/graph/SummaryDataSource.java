b8e578847e75b65fdb713bb1e8ab2118b4ff4c20
Chee Wai Lee <cheelee@illinois.edu>
2005-04-25 21:41:28 +0000

    Minor fix to the way foreground and background colors are handled for SummaryGraph data.

--- projections/gui/graph/SummaryDataSource.java
+++ projections/gui/graph/SummaryDataSource.java
@@ -5,53 +5,46 @@ import java.awt.Color;
 
 public class SummaryDataSource extends DataSource
 {
-  private int numIntervals;
-  private double dataValues[];
-
-    private Color graphColor = Color.white;
-  
-  public SummaryDataSource(double[] data) {
-    dataValues = data;
-    numIntervals = data.length;
-    //    System.out.println("SumDatSour "+numIntervals);
-  }
-
-  public String getTitle() { return "Utilization Graph (Summary)"; }
+    private int numIntervals;
+    private double dataValues[];
     
-  /**
-   * Return the number of indices (X axis points) in the problem.
-   */
-  public int getIndexCount() { return numIntervals; }
-  
-  /**
-   * Return the number of values (Y axis points) associated with each index.
-   */
-  public int getValueCount() { return 1; }
-  
-  public Color getColor(int valNo) { return graphColor; }
-  
-    public void setColor(Color newColor) {
-	graphColor = newColor;
+    public SummaryDataSource(double[] data) {
+	dataValues = data;
+	numIntervals = data.length;
+	//    System.out.println("SumDatSour "+numIntervals);
     }
+    
+    public String getTitle() { return "Utilization Graph (Summary)"; }
+    
+    /**
+     * Return the number of indices (X axis points) in the problem.
+     */
+    public int getIndexCount() { return numIntervals; }
+    
+    /**
+     * Return the number of values (Y axis points) associated with each index.
+     */
+    public int getValueCount() { return 1; }
+    
+    public Color getColor(int valNo) { return Analysis.foreground; }
 
-  /**
-   * Return the values associated with this index.
-   *  The index passed in is between 0 and getIndexCount()-1, inclusive.
-   *  The values array to be filled out is passed in, rather than returned
-   *    to reduce the dynamic allocation and garbage collection overhead.  
-   *  The values array has getValueCount() elements.
-   *
-   * Values[] will be sorted and plotted in the apropriate color along 
-   *   the Y axis.  To simulate a stacked bar graph, sum the values as
-   *   you fill in the array.  
-   * (FIXME: might want a "getStacked()" method instead)
-   */
-  public void getValues(int index, double[] values) {
-    values[0] = dataValues[index];
-  }
-  
-  public String[]getPopup(int xVal, int yVal){
+    /**
+     * Return the values associated with this index.
+     *  The index passed in is between 0 and getIndexCount()-1, inclusive.
+     *  The values array to be filled out is passed in, rather than returned
+     *    to reduce the dynamic allocation and garbage collection overhead.  
+     *  The values array has getValueCount() elements.
+     *
+     * Values[] will be sorted and plotted in the apropriate color along 
+     *   the Y axis.  To simulate a stacked bar graph, sum the values as
+     *   you fill in the array.  
+     * (FIXME: might want a "getStacked()" method instead)
+     */
+    public void getValues(int index, double[] values) {
+	values[0] = dataValues[index];
+    }
+    
+    public String[]getPopup(int xVal, int yVal){
    	return null;
-  }
+    }
 }
-

616568d3045bd226cab4726231b2e4d405cf184e
Chao Mei <chaomei2@illinois.edu>
2006-01-09 01:16:34 +0000

    Additional functionalities when dealing with AMPI functions

--- projections/analysis/AmpiFunctionData.java
+++ projections/analysis/AmpiFunctionData.java
@@ -1,8 +1,72 @@
 package projections.analysis;
+import java.util.Vector;
+
+import projections.gui.Analysis;
 
 public class AmpiFunctionData {
     // AMPI function tracing
     public int FunctionID, LineNo;
     public String sourceFileName;
+
+    private long accExecTime;
+
+    private Vector execIntervals;
+
+    public AmpiFunctionData() {
+        FunctionID = LineNo = 0;
+        sourceFileName = null;
+        accExecTime = 0;
+        execIntervals = new Vector();
+    }
+
+    public AmpiFunctionData(int funcId, int line, String srcFileName) {
+        FunctionID = funcId;
+        LineNo = line;
+        sourceFileName = srcFileName;
+        accExecTime = 0;
+        execIntervals = new Vector();
+    }
+
+    public void incrAccExecTime(long t) { accExecTime += t; }
+    public long getAccExecTime() { return accExecTime; }
+
+    public String toString(){
+        return sourceFileName+"@"+LineNo+"::"+FunctionID+" : "+accExecTime;
+    }
+
+    public Vector getExecIntervals(){
+        return execIntervals;
+    }
+
+    public void insertExecInterval(AmpiFuncExecInterval interval){
+        execIntervals.add(interval);
+    }
+
+    public AmpiFuncExecInterval getIntervalAt(int i){
+        return (AmpiFuncExecInterval)execIntervals.get(i);
+    }
+
+    public int execIntervalCnt(){
+        return execIntervals.size();
+    }
+
+    public String getFunctionName(){
+        String name = Analysis.getFunctionName(FunctionID);
+        return name+"@"+sourceFileName+"("+LineNo+")";
+    }
+
+    public static class AmpiFuncExecInterval {
+        public long startTimestamp;
+        public long endTimestamp;
+
+        public AmpiFuncExecInterval(long begin, long end){
+            startTimestamp = begin;
+            endTimestamp = end;
+        }
+        
+        public String toString(){
+            return "Start: "+startTimestamp+"; End: "+endTimestamp;
+        }
+    }
 }
 

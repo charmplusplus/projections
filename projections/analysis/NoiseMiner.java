038884ea0a11132f05bf29b7d234f75be6ff5d59
Isaac Dooley <idooley2@illinois.edu>
2007-04-11 05:40:00 +0000

    Added histograms for each entry method.

--- projections/analysis/NoiseMiner.java
+++ projections/analysis/NoiseMiner.java
@@ -25,6 +25,15 @@ import java.util.ArrayList;
  *  In order to be fast we will use little memory, and only take a single pass through the 
  *  provided data window.
  *  
+ *  
+ *  
+ *  The mining algorithm performs the following steps
+ *  
+ *  	maintain for each event a histogram 
+ *  	
+ *  
+ *  
+ *  
  */
 
 public class NoiseMiner extends ProjDefs
@@ -41,6 +50,56 @@ public class NoiseMiner extends ProjDefs
 	private long highWatermarkMemoryUsage;
 
 	private String loggingText;
+
+	private class Histogram{
+		private long bins[];
+		private int nbins;
+		private long total_sum;
+		private long total_count;
+		private boolean used;
+		
+		public Histogram(){ 
+			used=false;
+//			System.out.println("Constructor for Histogram");
+			nbins = 25;
+			bins = new long[nbins];
+			for(int i=0;i<nbins;i++){
+				bins[i] = 0;
+			}
+		}
+		
+		public void insert(long value){
+			used=true;
+			total_count ++;
+			total_sum += value;
+			int which_bin = (int)(value / 1000l);
+			if(which_bin > nbins-1){
+				which_bin = nbins-1;
+			}
+//			System.out.println("which_bin=" + which_bin);
+			bins[which_bin] ++;
+		}
+		
+		public String toString(){
+			String s ="";
+			if(used){
+				for(int i=0;i<nbins;i++){
+					s = s + bins[i] + ", ";
+				}
+			}
+			else{
+				s = "unused";
+			}
+				
+			return s;
+		}
+		
+		public boolean isUsed(){
+			return used;
+		}
+		
+	}
+	
 	
 	public NoiseMiner(long startInterval, long endInterval, 
 			OrderedIntList processorList)
@@ -54,7 +113,7 @@ public class NoiseMiner extends ProjDefs
 
 		baselineMemoryUsage = java.lang.Runtime.getRuntime().totalMemory();
 		highWatermarkMemoryUsage = baselineMemoryUsage;
-		
+
 		loggingText = "";
 	}
 
@@ -69,7 +128,7 @@ public class NoiseMiner extends ProjDefs
 		String s = "Max memory used by NoiseMiner: " + ((highWatermarkMemoryUsage - baselineMemoryUsage)/1024/1024) + " MB\n";
 		return s;
 	}
-	
+
 	public void gatherData(Component parent)
 	{
 		GenericLogReader LogFile;
@@ -78,7 +137,16 @@ public class NoiseMiner extends ProjDefs
 		int currPeIndex = 0;
 		int currPe;
 		int sourceEP;
-
+		
+		long previous_time=-1;
+		int  previous_entry=-1;
+		
+		int numHistograms = 250;
+		Histogram h[] = new Histogram[numHistograms];
+		for(int i=0;i<numHistograms;i++){
+			h[i] = new Histogram();	
+		}
+		
 		ProgressMonitor progressBar = new ProgressMonitor(parent, "Mining for Computational Noise","", 0, numPe);
 
 		// For each pe
@@ -93,51 +161,87 @@ public class NoiseMiner extends ProjDefs
 				break;
 			}
 			LogFile = new GenericLogReader(Analysis.getLogName(currPe), Analysis.getVersion());    
-		
+
 			int count=0;
 			try {
-			
+
 				LogFile.nextEventOnOrAfter(startTime, logdata);
 
 				while (logdata.time < endTime) {
-/*					//Go through each log file and for each Creation
+					//System.out.println("Time=" + logdata.time + " event " + logdata.event);
+					//Go through each log file and for each Creation
 					//  in a Processing Block, update arrays
-					if (logdata.type == BEGIN_PROCESSING) {
-						//Starting new entry method
-						sourceEP = logdata.entry;	
-						LogFile.nextEvent(logdata);
-						while ( (logdata.type != END_PROCESSING) && (logdata.type != END_COMPUTATION) && (logdata.time < endTime) ) {
-						
-							int msglen = logdata.msglen;
-							int destEP = logdata.entry;
-							loggingText = loggingText + "Log entry: Type=" + logdata.type + " len=" + msglen + " destEP=" + destEP + "\n";
-						
-							LogFile.nextEvent(logdata);
+					if(logdata.type == BEGIN_COMPUTATION){
+//						System.out.println("Found BEGIN_COMPUTATION");
+					} else if(logdata.type == END_COMPUTATION){
+//						System.out.println("Found END_COMPUTATION");
+					} else if(logdata.type == BEGIN_IDLE){
+//						System.out.println("Found BEGIN_IDLE");
+					} else if(logdata.type == END_IDLE){
+//						System.out.println("Found END_IDLE");
+					} else if(logdata.type == USER_EVENT_PAIR){
+//						System.out.println("Found USER_EVENT_PAIR");
+					} else if(logdata.type == CREATION){
+//						System.out.println("Found CREATION");
+					} else if(logdata.type == BEGIN_PROCESSING){
+//						 System.out.println("Found BEGIN_PROCESSING for event " + logdata.event + " at time " + logdata.time + " for entry method " + logdata.entry);
+						previous_time = logdata.time;
+						previous_entry = logdata.entry;
+					
+					} else if(logdata.type == END_PROCESSING){
+						// if we have seen the matching BEGIN_PROCESSING
+						if(previous_entry == logdata.entry){
+							long duration = logdata.time - previous_time;
+//							if(duration > 2000)
+//								System.out.println("Entry " + logdata.entry + " for event " + logdata.event + " took " + duration + " us");						
+							h[logdata.entry].insert(duration);
 						}
+						
+						
+//						 System.out.println("Found END_PROCESSING for event " + logdata.event + " at time " + logdata.time);
+					} else if(logdata.type == BEGIN_UNPACK){
+//						System.out.println("Found BEGIN_UNPACK");
+					} else if(logdata.type == END_UNPACK){
+//						System.out.println("Found END_UNPACK");
+					} else if(logdata.type == CREATION_MULTICAST){
+//						System.out.println("Found CREATION_MULTICAST");
+					} else if(logdata.type == BEGIN_PACK){
+//						System.out.println("Found BEGIN_PACK");
+					} else if(logdata.type == END_PACK){
+//						System.out.println("Found END_PACK");
+					} else if(logdata.type == USER_EVENT){
+//						System.out.println("Found USER_EVENT");
+					} else {
+//						System.out.println("Found unknown log entry #" + logdata.type);
 					}
-*/
-					count ++;
 
 					LogFile.nextEvent(logdata);
-					
+					count ++;
 				}
-			
-				
+
+
 			}
+
 			catch (IOException e)
 			{
 			}
-	
+
 			loggingText = loggingText + "Found " + count + " events in the specified time range on pe=" + currPe + "\n";
 			checkMemoryUsage();
 			currPeIndex++;
 		}	
 
+		for(int i=0;i<numHistograms;i++){
+			if(h[i].isUsed()){
+				System.out.println("Histograms for " + i + " is: " + h[i]);
+			}
+		}
 		progressBar.close();
 	}
 
 	public String getText(){
 		String s = "NoiseMiner Coming Soon!\n";
+		s = s + "Time range " + startTime + " to " + endTime + "\n";
 		s = s + loggingText + memoryUsageToString();
 		return s;
 	}

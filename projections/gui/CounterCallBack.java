77b75371bac1500a0d0028e439120658797f6e2b
Isaac Dooley <idooley2@illinois.edu>
2007-06-09 19:55:08 +0000

    Fixed hundreds of errors and warnings given by Eclipse includingRemoved unused code/variables, Removed catch for exceptions that are not thrown, Removed unused import statements, Removed unnecessary casts,  Commenting out unused files

--- projections/gui/CounterCallBack.java
+++ projections/gui/CounterCallBack.java
@@ -1,34 +1,36 @@
-package projections.gui;
+// UNUSED FILE
 
-import java.io.*;
-import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.table.*;
-import javax.swing.event.*;
-import projections.gui.count.*;
-
-public class CounterCallBack extends CallBack{
-	CounterFrame f;
-	ProjectionsFileChooser fc;
-	public CounterCallBack(CounterFrame f,ProjectionsFileChooser fc){
-		this.f = f;
-		this.fc = fc;
-	}
-	public void callBack(){
-	try{
-		ProjectionsFileMgr  fileMgr = fc.getProjectionsFileMgr();
-	  	fileMgr.printSts();
-     	 	f.setFileMgr(fileMgr);
-     	 	f.setSize(800,600);
-		//f.setVisible(true);
-     	 	f.loadFiles();
-     	 	f.sortByColumn(1);
-
-	}catch(Exception e){
-		System.out.println("Exception ");
-		e.printStackTrace();
-	}
-	}
-}
+//package projections.gui;
+//
+//import java.io.*;
+//import java.util.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import javax.swing.*;
+//import javax.swing.table.*;
+//import javax.swing.event.*;
+//import projections.gui.count.*;
+//
+//public class CounterCallBack extends CallBack{
+//	CounterFrame f;
+//	ProjectionsFileChooser fc;
+//	public CounterCallBack(CounterFrame f,ProjectionsFileChooser fc){
+//		this.f = f;
+//		this.fc = fc;
+//	}
+//	public void callBack(){
+//	try{
+//		ProjectionsFileMgr  fileMgr = fc.getProjectionsFileMgr();
+//	  	fileMgr.printSts();
+//     	 	f.setFileMgr(fileMgr);
+//     	 	f.setSize(800,600);
+//		//f.setVisible(true);
+//     	 	f.loadFiles();
+//     	 	f.sortByColumn(1);
+//
+//	}catch(Exception e){
+//		System.out.println("Exception ");
+//		e.printStackTrace();
+//	}
+//	}
+//}

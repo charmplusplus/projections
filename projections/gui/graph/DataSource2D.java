e30ee527dbc4971a4573c442a412ddf448e17ede
Chee Wai Lee <cheelee@illinois.edu>
2007-10-11 21:30:51 +0000

    Added summary statistics to graph-bars. Makes it somewhat easier to reason about graphs.

--- projections/gui/graph/DataSource2D.java
+++ projections/gui/graph/DataSource2D.java
@@ -45,12 +45,51 @@ public class DataSource2D extends DataSource
     }
 
     public String[] getPopup(int xVal, int yVal) {
-	if (parent != null) {
-	    if (parent instanceof PopUpAble) {
-		return ((PopUpAble)parent).getPopup(xVal, yVal);
+      int NUM_EXTRA_LINES = 4;
+      if (parent != null) {
+	if (parent instanceof PopUpAble) {
+	  String []tempText = ((PopUpAble)parent).getPopup(xVal, yVal);
+	  int offset = tempText.length;
+	  int longestLength = 0;
+	  String []text = new String[offset + NUM_EXTRA_LINES];
+	  if (offset == 0) {
+	    return tempText;
+	  } else {
+	    for (int i=0; i<offset; i++) {
+	      if (tempText[i].length() > longestLength) {
+		longestLength = tempText[i].length();
+	      }
+	      text[i] = tempText[i];
 	    }
-	} 
-	return null;		
+	    // Draw a line
+	    text[offset] = new String();
+	    for (int i=0; i<longestLength; i++) {
+	      text[offset] += "-";
+	    }
+	    // Compute Statistics
+	    int count = 0;
+	    double total = 0.0;
+	    for (int i=0; i<data[xVal].length; i++) {
+	      if (data[xVal][i] > 0.0) {
+		count++;
+		total += data[xVal][i];
+	      }
+	    }
+	    // Display number of non-zero entries
+	    //         average value
+	    //         total value
+	    text[offset+1] = "Number: " + count;
+	    if (count > 0) {
+	      text[offset+2] = "Average: " + total/count;
+	    } else {
+	      text[offset+2] = "No Average";
+	    }
+	    text[offset+3] = "Total: " + total;
+	  }
+	  return text;
+	}
+      } 
+      return null;		
     }
     
     public void toolClickResponse(MouseEvent e, int xVal, int yVal) {

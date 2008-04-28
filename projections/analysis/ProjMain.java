7314d83a388205d1f027132556bd9b64bc11e121
Isaac Dooley <idooley2@illinois.edu>
2008-04-28 19:24:36 +0000

    now a dialog box is displayed at startup if no command line arguments are given. It will search the current directory for any sts files, prompting the user to open one of the sts files it finds. It ignores an ".sum.sts" files it encounters. It will display a dialog box for each sts file found until the user clicks "yes" for one of them.

--- projections/analysis/ProjMain.java
+++ projections/analysis/ProjMain.java
@@ -1,5 +1,9 @@
 package projections.analysis;
 
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
 import projections.gui.MainWindow;
 
 /**
@@ -67,65 +71,93 @@ public class ProjMain {
     }
 
     public static void main(String args[])
-    {
-        int i=0;
-	String loadSts=null;
-        while (i < args.length) {
-	    if (args[i].equals("-h") ||
-		args[i].equals("--help")) {
-		help();
-	    }
-	    else if (args[i].equals("-V") ||
-		     args[i].equals("--version")) {
-		System.out.println("Projections version: " + CUR_VERSION);
-		System.exit(0);
-	    }
-	    else if (args[i].equals("-u") ||
-		     args[i].equals("-use-version")) {
-		i++;
-		if (i==args.length) help();
-		double useVersion = Double.parseDouble(args[i]);
-		if (useVersion > CUR_VERSION) {
-		    System.out.println("Unable to use version " + useVersion +
-				       "! Maximum supported version on " +
-				       "this binary is " + CUR_VERSION + ".");
-		    System.exit(1);
-		}
-		CUR_VERSION = useVersion;
-	    } else if (args[i].equals("-no-idle")) {
-		IGNORE_IDLE = true;
-	    } else if (args[i].equals("-bgsize")) {
-		i++;
-		BLUEGENE_SIZE[0] = Integer.parseInt(args[i]);
-		i++;
-		BLUEGENE_SIZE[1] = Integer.parseInt(args[i]);
-		i++;
-		BLUEGENE_SIZE[2] = Integer.parseInt(args[i]);
-		BLUEGENE = true;
-	    } else if (args[i].equals("-summary")) {
-		i++;
-		SUM_START_INT = Integer.parseInt(args[i]);
-		i++;
-		SUM_END_INT = Integer.parseInt(args[i]);
-		i++;
-		SUM_INT_SIZE = Long.parseLong(args[i]);
-		SUM_OVERRIDE = true;
-	    } else if (args[i].equals("-print_usage")) {
-		PRINT_USAGE = true;
-	    } else /* Expected Sts Filename */ {
-		loadSts=args[i];
-	    }
-	    i++;
-	}
+    {	
+    	/// The sts file to load
+    	String loadSts=null;
+    	boolean done = false;
+    	// If no sts file is given, then we will search in the current directory to find sts files
+    	if(args.length == 0){
+    		File currentdir =new File (".");
+    		File [] allChildren = currentdir.listFiles();
+    		if(allChildren != null){
+    			for(int i=0;i<allChildren.length && !done;i++){
+    				File child = allChildren[i];
+    				if(child.isFile()){
+    					String name = child.getName();
+    					if(name.endsWith(".sts") && ! name.endsWith(".sum.sts") ){
+    						int choice = JOptionPane.showConfirmDialog(null, 
+	    								 new String("<html><body>You didn't specify a file to open.<br>Do you want to open <b>" + name + "</b> from the current directory?</body></html>"),
+	    								 new String("Question"),
+	    								 JOptionPane.YES_NO_OPTION);
+    						if(choice == JOptionPane.YES_OPTION){
+    							loadSts = child.getAbsolutePath();
+    							done = true;
+    						} 
+    					}
+    				}
+    			}
+    		}
 
-	mainWindow = new MainWindow();
-	mainWindow.pack();
-	mainWindow.setTitle("Projections");
-	mainWindow.setVisible(true);
+    	}
+    	
+    	int i=0;
 
-	// Load Data if specified on command line
-	if (loadSts!=null) { 
-	    mainWindow.openFile(loadSts); 
-	}
+    	while (i < args.length) {
+    		if (args[i].equals("-h") ||
+    				args[i].equals("--help")) {
+    			help();
+    		}
+    		else if (args[i].equals("-V") ||
+    				args[i].equals("--version")) {
+    			System.out.println("Projections version: " + CUR_VERSION);
+    			System.exit(0);
+    		}
+    		else if (args[i].equals("-u") ||
+    				args[i].equals("-use-version")) {
+    			i++;
+    			if (i==args.length) help();
+    			double useVersion = Double.parseDouble(args[i]);
+    			if (useVersion > CUR_VERSION) {
+    				System.out.println("Unable to use version " + useVersion +
+    						"! Maximum supported version on " +
+    						"this binary is " + CUR_VERSION + ".");
+    				System.exit(1);
+    			}
+    			CUR_VERSION = useVersion;
+    		} else if (args[i].equals("-no-idle")) {
+    			IGNORE_IDLE = true;
+    		} else if (args[i].equals("-bgsize")) {
+    			i++;
+    			BLUEGENE_SIZE[0] = Integer.parseInt(args[i]);
+    			i++;
+    			BLUEGENE_SIZE[1] = Integer.parseInt(args[i]);
+    			i++;
+    			BLUEGENE_SIZE[2] = Integer.parseInt(args[i]);
+    			BLUEGENE = true;
+    		} else if (args[i].equals("-summary")) {
+    			i++;
+    			SUM_START_INT = Integer.parseInt(args[i]);
+    			i++;
+    			SUM_END_INT = Integer.parseInt(args[i]);
+    			i++;
+    			SUM_INT_SIZE = Long.parseLong(args[i]);
+    			SUM_OVERRIDE = true;
+    		} else if (args[i].equals("-print_usage")) {
+    			PRINT_USAGE = true;
+    		} else /* Expected Sts Filename */ {
+    			loadSts=args[i];
+    		}
+    		i++;
+    	}
+
+    	mainWindow = new MainWindow();
+    	mainWindow.pack();
+    	mainWindow.setTitle("Projections");
+    	mainWindow.setVisible(true);
+
+    	// Load Data if specified on command line
+    	if (loadSts!=null) { 
+    		mainWindow.openFile(loadSts); 
+    	}
     }
 }

31cc92a74459a716a7c056cb7bdd12e9901376ae
Isaac Dooley <idooley2@illinois.edu>
2009-11-03 19:12:55 +0000

    Fixing the routine that determines the end times across all log files. Now it scans through all log entries, not assuming they are in sorted order. Furthermore, this version uses GenericLogReader so it can handle compressed log files.

--- projections/analysis/LogLoaderEndTimeThread.java
+++ projections/analysis/LogLoaderEndTimeThread.java
@@ -1,14 +1,12 @@
 package projections.analysis;
 
 import java.io.EOFException;
-import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.StringTokenizer;
 
-import javax.swing.ProgressMonitor;
-
 import projections.gui.MainWindow;
+import projections.misc.LogEntryData;
 
 
 /** This thread's run() method will lookup the endtime for an input log file */
@@ -16,85 +14,29 @@ public class LogLoaderEndTimeThread  extends Thread {
 
 	public String logName;
 	public Long result;
+	int myRun = 0;
 
 	public LogLoaderEndTimeThread(String  _logName) {
 		result = new Long(0);
 		logName = _logName;
+		myRun = 0;
 	}
 
+	/** Find the end time for the given logfile	*/
 	public void run() {
-
-		String           Line;
-		RandomAccessFile InFile;
-		StringTokenizer  st;
-
-		// Find the begin and end time for the given logfile
-
-		long dummyLong = 0;
-		int type = 0;
-		try {
-			InFile =  new RandomAccessFile(logName, "r");
-
-			long back = InFile.length()-100*3; //Seek to the end of the file
-			if (back < 0) back = 0;
-			InFile.seek(back);
-			while (InFile.readByte() != '\n'){}
+		try {	  
+			GenericLogReader reader = new GenericLogReader(logName, MainWindow.runObject[myRun].getVersion());
 			while (true) {
-				Line = InFile.readLine();
-				// incomplete files can end on a proper previous
-				// line but not on END_COMPUTATION. This statement
-				// takes care of that.
-				if (Line == null) {
-					throw new EOFException();
-				}
-				st = new StringTokenizer(Line);
-				if ((type=Integer.parseInt(st.nextToken())) == ProjDefs.END_COMPUTATION) {
-					long time = Long.parseLong(st.nextToken());
-					if (time > result)
-						result = time;
-					break; // while loop
-				} else {
-					switch (type) {
-					case ProjDefs.CREATION:
-					case ProjDefs.CREATION_BCAST:
-					case ProjDefs.CREATION_MULTICAST:
-					case ProjDefs.BEGIN_PROCESSING:
-					case ProjDefs.END_PROCESSING:
-						dummyLong = Integer.parseInt(st.nextToken());
-						dummyLong = Integer.parseInt(st.nextToken());
-						break;
-					case ProjDefs.USER_SUPPLIED:
-					case ProjDefs.MEMORY_USAGE:
-					case ProjDefs.USER_EVENT:
-					case ProjDefs.USER_EVENT_PAIR:
-					case ProjDefs.MESSAGE_RECV: 
-					case ProjDefs.ENQUEUE: 
-					case ProjDefs.DEQUEUE:
-						dummyLong = 	Integer.parseInt(st.nextToken());
-						break;
-					case ProjDefs.BEGIN_IDLE:
-					case ProjDefs.END_IDLE:
-					case ProjDefs.BEGIN_PACK: 
-					case ProjDefs.END_PACK:
-					case ProjDefs.BEGIN_UNPACK: 
-					case ProjDefs.END_UNPACK:
-					case ProjDefs.BEGIN_TRACE: 
-					case ProjDefs.END_TRACE:
-					case ProjDefs.BEGIN_COMPUTATION:
-					case ProjDefs.BEGIN_FUNC:
-					case ProjDefs.END_FUNC:
-					case ProjDefs.BEGIN_INTERRUPT: 
-					case ProjDefs.END_INTERRUPT:
-						break;
-
-					}
-				}
+				LogEntryData data = reader.nextEvent();
+				if (data.time > result)
+					result = data.time;
 			}
-			InFile.close ();
-		} catch (IOException E) {
-			System.err.println("Couldn't read log file " + logName);
-		}
-		
+		} catch (EOFException e) {
+			// finished reading the file
+		} catch (IOException e) {
+			// Some error occured
+			System.err.println("Error occurred while reading from " + logName);
+		}		
 	}
 
 }

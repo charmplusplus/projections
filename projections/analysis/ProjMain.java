a2e0d988ae0089d94a22589d5111112a7935b1ac
Chee Wai Lee <cheewai1972@gmail.com>
2009-12-08 19:50:03 -0600

    Made sure the initial sts filename carried the full path name or Isaac's subsequent code would break.

--- projections/analysis/ProjMain.java
+++ projections/analysis/ProjMain.java
@@ -162,7 +162,17 @@ public class ProjMain {
 
     	// Load Data if specified on command line
     	if (loadSts!=null) { 
-    		mainWindow.openFile(loadSts); 
+	    // Isaac has added code that require full path names later.
+	    //   Use a heuristic to determine if loadSts is an absolute
+	    //   path. If not, prefix it with the current working 
+	    //   directory.
+	    String curDir = System.getProperty("user.dir");
+	    int index = loadSts.lastIndexOf(File.separator);
+	    if (index == 0) {
+		mainWindow.openFile(loadSts); 
+	    } else {
+		mainWindow.openFile(curDir + File.separator + loadSts); 
+	    }
     	}
     }
 }

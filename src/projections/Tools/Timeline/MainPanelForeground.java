e88a9899b9bbbcfeb043b7b71448016b2119c66c
Isaac Dooley <isaacdooley@hope.cs.uiuc.edu>
2010-01-21 16:21:58 -0600

    Made message lines thicker so they will show up for plots.

--- src/projections/Tools/Timeline/MainPanelForeground.java
+++ src/projections/Tools/Timeline/MainPanelForeground.java
@@ -1,7 +1,9 @@
 package projections.Tools.Timeline;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -34,16 +36,16 @@ class MainPanelForeground extends JPanel {
 		// (including background filling, if I'm opaque)
 		super.paintComponent(g); 
 		// paint the message send lines	
-		paintMessageSendLines(g, data.getMessageColor(), data.drawMessagesForTheseObjects);
-		paintMessageSendLines(g, data.getMessageAltColor(), data.drawMessagesForTheseObjectsAlt);	
+		paintMessageSendLines(g, data.getMessageColor(), data.getBackgroundColor(), data.drawMessagesForTheseObjects);
+		paintMessageSendLines(g, data.getMessageAltColor(), data.getBackgroundColor(), data.drawMessagesForTheseObjectsAlt);	
 	}
 
 
 	
-	private void paintMessageSendLines(Graphics g, Color c, Set drawMessagesForObjects){
+	private void paintMessageSendLines(Graphics g, Color c, Color bgColor, Set drawMessagesForObjects){
+		Graphics2D g2d = (Graphics2D) g;
 		// paint the message send lines
 		if (drawMessagesForObjects.size()>0) {
-			g.setColor(c);
 			Iterator iter = drawMessagesForObjects.iterator();
 			while(iter.hasNext()){
 				Object o = iter.next();
@@ -60,7 +62,13 @@ class MainPanelForeground extends JPanel {
 						int x2 =  data.timeToScreenPixel(obj.getBeginTime());
 						double y2 = data.messageRecvLocationY(pExecution);
 
-						g.drawLine(x1,(int)y1,x2,(int)y2);
+						// Draw thick background Then thin foreground
+						g2d.setPaint(bgColor);
+						g2d.setStroke(new BasicStroke(4.0f));
+						g2d.drawLine(x1,(int)y1,x2,(int)y2);
+						g2d.setPaint(c);
+						g2d.setStroke(new BasicStroke(2.0f));
+						g2d.drawLine(x1,(int)y1,x2,(int)y2);
 					}
 				}
 			}

8f603bf2fb66ea2482a9f709138342b5558c5de5
Tianyi Wang <wang103@illinois.edu>
2012-04-11 04:07:04 -0500

    Add more UI initialization code. Add the 3 j3d jar files to the projections.jar without their META-INFO to avoid runtime SecurityException about invalid signature

--- src/projections/Tools/TopologyDisplay/TopologyDisplayWindow.java
+++ src/projections/Tools/TopologyDisplay/TopologyDisplayWindow.java
@@ -3,9 +3,18 @@ package projections.Tools.TopologyDisplay;
 import projections.gui.MainWindow;
 import projections.gui.ProjectionsWindow;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.TransformGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.vecmath.Color3f;
 
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
@@ -21,7 +30,8 @@ import com.sun.j3d.utils.universe.SimpleUniverse;
  * 
  * @author wang103
  */
-public class TopologyDisplayWindow extends ProjectionsWindow {
+public class TopologyDisplayWindow extends ProjectionsWindow 
+		implements ItemListener {
  	
 	static final float pointRadius = 0.13f;
 	static final float coneRadius = 0.12f;
@@ -30,7 +40,6 @@ public class TopologyDisplayWindow extends ProjectionsWindow {
 	private Appearance redAppearance;
 	private Appearance greenAppearance;
 	private Appearance blueAppearance;
-	private Appearance yellowAppearance;
 
 	private SimpleUniverse univserse;
 	private BranchGroup scene;
@@ -44,8 +53,70 @@ public class TopologyDisplayWindow extends ProjectionsWindow {
 
 	/************* Initialization *************/
 
+	public void initUI() {
+		JMenuBar menuBar = new JMenuBar();
+
+		// set file menu
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+
+		JMenuItem inputFileItem = new JMenuItem("Input File");
+		inputFileItem.addActionListener(this);
+		fileMenu.add(inputFileItem);
+
+		JMenuItem quitItem = new JMenuItem("Quit");
+		quitItem.addActionListener(this);
+		fileMenu.add(quitItem);
+
+		// set options menu
+		JMenu optionsMenu = new JMenu("Options");
+		menuBar.add(optionsMenu);
+
+		JCheckBoxMenuItem showBoxItem = new JCheckBoxMenuItem("Show Box");
+		showBoxItem.setSelected(true);
+		showBoxItem.addItemListener(this);
+		optionsMenu.add(showBoxItem);
+
+		JCheckBoxMenuItem showCoordItem = new JCheckBoxMenuItem("Show Coordinates");
+		showCoordItem.addItemListener(this);
+		optionsMenu.add(showCoordItem);
+
+		// set screenshot menu
+		JMenu screenshotMenu = new JMenu("Screenshot");
+		menuBar.add(screenshotMenu);
+
+		JMenuItem screenshotItem = new JMenuItem("Take Screenshot");
+		screenshotItem.addActionListener(this);
+		screenshotMenu.add(screenshotItem);
+
+		this.setJMenuBar(menuBar);
+	}
+
+	public void initCommonAppearance() {
+		// red appearance
+		redAppearance = new Appearance();
+		Color3f redColor = new Color3f(1.0f, 0.0f, 0.0f);
+		ColoringAttributes redCA = new ColoringAttributes(redColor, 1);
+		redAppearance.setColoringAttributes(redCA);
+
+		// green appearance
+		greenAppearance = new Appearance();
+		Color3f greenColor = new Color3f(0.0f, 1.0f, 0.0f);
+		ColoringAttributes greenCA = new ColoringAttributes(greenColor, 1);
+		greenAppearance.setColoringAttributes(greenCA);
+
+		// blue appearance
+		blueAppearance = new Appearance();
+		Color3f blueColor = new Color3f(0.0f, 0.0f, 1.0f);
+		ColoringAttributes blueCA = new ColoringAttributes(blueColor, 1);
+		blueAppearance.setColoringAttributes(blueCA);
+	}
+
 	public TopologyDisplayWindow(MainWindow parentWindow) {
 		super(parentWindow);
+
+		setTitle("Projections 3D Topology");
+		setSize(500, 500);
 
 		maxX = 0;
 		minX = 0;
@@ -53,12 +124,15 @@ public class TopologyDisplayWindow extends ProjectionsWindow {
 		minY = 0;
 		maxZ = 0;
 		minZ = 0;
-		
+
+		this.initCommonAppearance();
+		this.initUI();
+
 		showDialog();
 	}
 
 	protected void showDialog() {
-	
+		this.setVisible(true);
 	}
 
 
@@ -73,6 +147,15 @@ public class TopologyDisplayWindow extends ProjectionsWindow {
 
 	/************* Implemented Listeners *************/
 
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		JCheckBoxMenuItem source = (JCheckBoxMenuItem) e.getSource();
 
+		if (source.getText().equals("Show Box")) {
+		
+		} else if (source.getText().equals("Show Coordinates")) {
+		
+		}
+	}
 }
 

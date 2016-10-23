f27e8a5f39f4a0a82ea2bd57898ea2b46327c758
Ronak Buch <rabuch2@illinois.edu>
2016-10-23 18:33:25 -0500

    Change MergedIterator to use array instead of LinkedList

--- src/projections/Tools/Timeline/RangeQueries/MergedIterator.java
+++ src/projections/Tools/Timeline/RangeQueries/MergedIterator.java
@@ -5,39 +5,30 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class MergedIterator<E> implements Iterator<E>{
-	private LinkedList<Iterator<E>> backingIterators;
-	
-	public MergedIterator(Iterator<E> i1, Iterator<E> i2){
-		backingIterators = new LinkedList<Iterator<E>>();
-		backingIterators.add(i1);
-		backingIterators.add(i2);
+	private final Iterator<E> iterators[];
+	private int index;
+
+	public MergedIterator(Iterator<E>... iters){
+		iterators = iters;
+		index = 0;
 	}
 	
 	
 	@Override
 	public boolean hasNext() {
-		while(! backingIterators.isEmpty()){
-			Iterator<E> i = backingIterators.getFirst();
-			if(i.hasNext()) {
-				return true;
-			} else {
-				backingIterators.removeFirst();
-			}			
+		while (index < iterators.length && !iterators[index].hasNext()) {
+			index++;
 		}
-		
-		return false;
+
+		return index < iterators.length;
 	}
 
 	@Override
 	public E next() {
-		while(! backingIterators.isEmpty()){
-			Iterator<E> i = backingIterators.getFirst();
-			if(i.hasNext()) {
-				return i.next();
-			} else {
-				backingIterators.removeFirst();
-			}			
+		if (hasNext()) {
+			return iterators[index].next();
 		}
+
 		throw new NoSuchElementException();
 	}
 

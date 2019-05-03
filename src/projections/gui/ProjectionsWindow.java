c72155f294a8c4993b12df350ce03e94ec8d6778
Matthias Diener <mdiener@illinois.edu>
2019-05-03 14:52:26 -0500

    test
    Change-Id: I2dd7e7ff2916120210b3902cabf3190b7046d5eb

--- src/projections/gui/ProjectionsWindow.java
+++ src/projections/gui/ProjectionsWindow.java
@@ -7,10 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JFrame;
 
 /**
  *  ProjectionsWindow
